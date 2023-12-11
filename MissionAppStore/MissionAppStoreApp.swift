61fa3383b7899f0c8606a06c3f2bd14a0d647e29
Quentin Cornu <contact@developpeurlibre.com>
2023-12-11 08:57:40 +0100

    Initial commit

---
+++ MissionAppStore/MissionAppStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  MissionAppStoreApp.swift
+//  MissionAppStore
+//
+//  Created by Quentin Cornu on 11/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct MissionAppStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
