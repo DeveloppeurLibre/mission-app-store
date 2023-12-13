2ae345edd154f7de9cb2e889b2e63b7860fee782
Quentin Cornu <contact@developpeurlibre.com>
2023-12-13 19:18:59 +0100

    Correction défi jour 7

--- MissionAppStore/ContentView.swift
+++ MissionAppStore/ContentView.swift
@@ -13,9 +13,17 @@ struct ContentView: View {
     
     var body: some View {
         HStack {
-            Circle()
-                .frame(width: 80)
-                .foregroundStyle(Color.gray)
+            AsyncImage(url: URL(string: "https://images.pexels.com/photos/2589653/pexels-photo-2589653.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")) { profilePicture in
+                profilePicture
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .clipShape(Circle())
+            } placeholder: {
+                Circle()
+                    .foregroundStyle(Color.gray)
+            }
+            .frame(width: 80, height: 80)
+
             VStack(alignment: .leading) {
                 Text("Quentin")
                     .bold()
@@ -23,8 +31,16 @@ struct ContentView: View {
                 Text("245 abonnés")
             }
             Spacer()
-            Text("S'abonner")
-                .foregroundStyle(Color.blue)
+            Button(action: {
+                // Action...
+            }, label: {
+                Text("S'abonner")
+                    .foregroundStyle(Color.white)
+                    .padding(.horizontal, 8)
+                    .padding(.vertical, 4)
+                    .background(Color.blue)
+                    .cornerRadius(8)
+            })
         }
         .padding()
     }
