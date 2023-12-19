fd9f116a31ca4f14f38069e1261ddf9d1f6e86b3
Quentin Cornu <contact@developpeurlibre.com>
2023-12-19 16:32:18 +0100

    Correction Jour 12

---
+++ MissionAppStore/Jour12Correction/Car.swift
@@ -0,0 +1,14 @@
+//
+//  Car.swift
+//  MissionAppStore
+//
+//  Created by Quentin Cornu on 19/12/2023.
+//
+
+import SwiftUI
+
+struct Car: Identifiable {
+    let id = UUID()
+    let model: String
+    let color: Color
+}
